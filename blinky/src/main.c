3579b68949149e8d2e71279e893e1e91ecc7ad9e
byte-engineer <blalalasmayly1998@gmail.com>
2026-03-19 00:28:08 +0400

    added blinky

--- blinky/src/main.c
+++ blinky/src/main.c
@@ -1,20 +1,17 @@
 #define F_CPU 16000000UL
-#define __AVR_ATMEGA__
+#define __AVR_ATMEGA8__
 
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main(void)
 {
-    // Set PB5 as output
     DDRB |= (1 << PB5);
 
     while (1)
     {
-        // Toggle LED
         PORTB ^= (1 << PB5);
 
-        // Wait 500 ms
         _delay_ms(500);
     }
 }
