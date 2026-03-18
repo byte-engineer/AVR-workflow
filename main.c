cbb64caeb9cf5f6a53b7a99fa5c8277036922336
byte-engineer <blalalasmayly1998@gmail.com>
2026-03-18 14:43:17 +0400

    Great running

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
+#define F_CPU 16000000UL
+#define __AVR_ATMEGA__
+
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void) {
-  // Set PB5 as output
-  DDRB |= (1 << PB5);
+int main(void)
+{
+    // Set PB5 as output
+    DDRB |= (1 << PB5);
 
-  while (1) {
-    // Toggle LED
-    PORTB ^= (1 << PB5);
+    while (1)
+    {
+        // Toggle LED
+        PORTB ^= (1 << PB5);
 
-    // Wait 500 ms
-    _delay_ms(500);
-  }
+        // Wait 500 ms
+        _delay_ms(500);
+    }
 }
